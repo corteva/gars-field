2d11a32bbddba820f508b6eb3c723218de8807c5
Alan D. Snow <alansnow21@gmail.com>
2022-10-31 10:27:44 -0500

    MNT: Use setuptools to find version in setup.cfg (#11)

--- gars_field/__init__.py
+++ gars_field/__init__.py
@@ -1,6 +1,8 @@
 """
 GARS Field is a toolkit for generating Global Area Reference grids
 """
+__version__ = "0.3.1.dev0"
+
 from .edgarsgrid import EDGARSGrid  # noqa: F401
 from .field import GARSField  # noqa: F401
 from .garsgrid import GARSGrid  # noqa: F401
